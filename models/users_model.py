80540877a7130fddc55e41c75806bd5c39741bc7
GrahamJReid <grahamreidtechnology@gmail.com>
2023-05-10 13:50:30 -0500

    get all  users functional

---
+++ models/users_model.py
@@ -0,0 +1,17 @@
+class Users():
+
+    # Class initializer. It has 5 custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, id, first_name, last_name,email,bio,username,password,profile_image_url,created_on,active):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.bio = bio
+        self.username = username
+        self.password = password
+        self.profile_image_url = profile_image_url
+        self.created_on = created_on
+        self.active = active
+       
